cac2f0d401db40fbe1c5a4af3857b5aaeb4dc77f
GustGimenez <gustavogdd2@gmail.com>
2018-02-26 23:01:59 -0300

    modal-sugerir-coleção
    Alterado o model de sugesstão de coleção, tá mais legalzinho.

--- application/views/usual/menu_usual.php
+++ application/views/usual/menu_usual.php
@@ -1,18 +1,17 @@
 <style>
-  
-  a.navbar-brand{
-    padding-right: 0px;
-  }
 
-  .imagem{
-    margin: 0px;
-    padding-top: 5px;
-  }
+a.navbar-brand{
+  padding-right: 0px;
+}
+
+.imagem{
+  margin: 0px;
+  padding-top: 5px;
+}
 
 </style>
 
 <nav class="navbar navbar-default">
-
   <div class="container-fluid">
     <div class="navbar-header">
       <img class="imagem" src="http://localhost/geoportal/imagens/icone.png">
@@ -22,9 +21,9 @@
     </div>
 
     <div class="nav navbar-nav">
-      <li><a onclick="document.getElementById('modalColecao').style.display='block'" style="width:auto;"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Sugerir Coleção</a></li>
       <li><a href="<?= base_url('usual/nova_marcacao')?>"><span class="glyphicon glyphicon-map-marker" aria-hidden="true"></span> Sugerir Marcação</a></li>
       <li><a href="<?=base_url('usual/listar_colecoes')?>"><span class="glyphicon glyphicon-globe" aria-hidden="true"></span> Escolher Coleção</a></li>
+      <li><button class="btn btn-info navbar-btn" onclick="document.getElementById('modalColecao').style.display='block'" style="width:auto;"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> Sugerir Colecao</button></li>
     </div>
     
     <ul class="nav navbar-nav navbar-right">
@@ -36,5 +35,4 @@
     </ul>
 
   </div>
-
 </nav>
--- application/views/usual/nova_colecao_model.php
+++ application/views/usual/nova_colecao_model.php
@@ -1,116 +1,30 @@
-<style>
-    
-    .atributosColacao{
-        height: 30px;
-        width: 5px;
-    }
-
-    .criar{
-        margin-left: 18%;
-        width: 100px;
-        height: 40px;
-        border-radius: 20px 20px 20px 20px;
-        padding: 5px;
-        border: 0;
-        display: inline-block;
-        background-color: limegreen;
-        color: white;
-    }
-
-    .criar:hover{
-        background-color: lime;
-    }
-
-    .cancelar{
-        padding-left: 2%;
-        width: 100px;
-        height: 40px;
-        border-radius: 20px 20px 20px 20px;
-        padding: 5px;
-        border: 0;
-        display: inline-block;
-        background-color: red;
-        color: white;
-    }
-
-    .cancelar:hover{
-        background-color: #FF0007;
-    }
-
-    .fonteTitulo{
-        font-size: 35px;
-        text-align: center;
-    }
-
-    .fonteDiversa{
-        font-size: 25px;
-        padding-top: 25px;
-    }
-
-    .fonteDiversaSenha{
-        font-size: 25px;
-        padding-top: 0px;
-    }
-
-    #nome{
-        border-style: none;
-        border-bottom-style: solid;
-        border-color: black;
-        background-color: azure;
-        margin-top: 0px;
-        width: 50%;
-    }
-
-    #descricao{
-        border-style: none;
-        border-bottom-style: solid;
-        border-color: black;
-        background-color: azure;
-        margin-top: 0px;
-        width: 50%;
-    }
-
-    #senha{
-        border-style: none;
-        border-bottom-style: solid;
-        border-color: black;
-        background-color: azure;
-        margin-top: 0px;
-        width: 50%;
-        padding-top: 0px;
-    }
-
-    .privada{
-        font-size: 16px;
-        padding-bottom: 0px;
-    }
-
-</style>
-
 <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.2.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="<?= base_url('assets/css/estilo-modal.css') ?>">
 
 <div id='modalColecao' class='modal'>
-   <form class="modal-content animate" method="post" action="<?= base_url('colecao/adicionar')?>">
-
-    <div class="container">
-        <label class="fonteTitulo"><b>Nome da Coleção</b></label></br>
-        <input class="atributosColacao" type="text" name="nome" id="nome" required></br>
-        <label class="fonteDiversa"><b>Descrição</b></label></br>
-        <input class="atributosColacao" type="text" name="descricao" id="descricao" required>
-        <br>
-        <br>
-        <div class="checkbox">
-            <input type="checkbox" name="privada" id="privada" onchange="mudar_senha(this)"> Privada
+    <form class="modal-content animate" method="post" action="<?= base_url('colecao/adicionar')?>">
+        <div id="container1">
+            <div class="titulo">
+                <h2>Criar Coleção</h2>
+            </div>
+            <label class="fonte"><b>Nome</b></label><br>
+            <input class="atributosColacao" type="text" name="nome" id="nome" required></br>
+            <label class="fonte"><b>Descrição</b></label></br>
+            <input class="atributosColacao" type="text" name="descricao" id="descricao" required><br>
+            <div class="checkbox">
+                <label style="margin-left: 10px; font-size: 15px">
+                    <input type="checkbox" name="privada" id="privada" onchange="mudar_senha(this)"> Privada
+                </label>
+            </div>
+            <label class="fonte"><b>Senha</b></label></br>
+            <input class="atributosColacao" type="password" name="senha" id="senha" disabled="true">
         </div>
-        <label class="fonteDiversaSenha"><b>Senha</b></label></br>
-        <input class="atributosColacao" type="password" name="senha" id="senha" disabled="true">
-    </div>
 
-    <div class="container">
-        <button class="criar" type="submit">Criar</button>
-        <button class="cancelar" type="button" onclick="document.getElementById('modalColecao').style.display='none'" 
-        class="cancelbtn">Cancelar</button>
-    </div>
+        <div class="botoes">
+            <button class="criar" type="submit">Criar</button>
+            <button class="cancelar" type="button" onclick="document.getElementById('modalColecao').style.display='none'" 
+            class="cancelbtn">Cancelar</button>
+        </div>
     </form>
 </div>
 <script>
