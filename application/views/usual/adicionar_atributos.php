200b96fd31e4533d6ae71d66c34079918a3679e7
GustGimenez <gustavogdd2@gmail.com>
2018-02-27 11:04:52 -0300

    adicionar-atributos
    Interface da página de adição de atributos para uma nova coleção está reformada. Agora funcionam os botões de adição e remoção dos campos de novos atributos.

--- application/views/usual/adicionar_atributos.php
+++ application/views/usual/adicionar_atributos.php
@@ -1,46 +1,18 @@
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-<link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" type="text/css">
-<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet" type="text/css">
-<!-- Formatacao de ES -->
-
-<style>
-    .my_button_plus{
-        background-color: white;
-        display: inline-block;
-        border-radius: 8px;
-        border: 2px solid #e7e7e7;
-        height: 32px;
-    }
-    .my_button_minus{
-        background-color: white;
-        display: inline-block;
-        border-radius: 8px;
-        border: 2px solid #e7e7e7;
-        height: 32px;
-    }
-
-    h1{
-        text-align: center;
-        size: 20px;
-    }
-
-</style>
-
-
+<link rel="stylesheet" href="<?= base_url('assets/css/estilo-adicionar_atributos.css') ?>">
 
 <div>
-    <h1> Escolha os Atributos </h1>
+    <h1>Escolha os Atributos</h1>
 </div>
 
 <form id="atributosFomr" method="post" action="<?= base_url('atributo/adicionar_atributos')?>">
 
-    <div class="form-group">
+    <div class="form-group" id="div_atributos">
         <div class="col-md-6">
             <input type="text" class="form-control" name="nome1" id="nome1" placeholder="Nome"/>
         </div>
 
         <div class="col-xs-3">
-            <select class="form-control" id="tipo" name="tipo1" id="tipo1">
+            <select class="form-control" name="tipo1" id="tipo1">
                 <option>INT</option>
                 <option>VARCHAR[ ]</option>
                 <option>BOOLEAN</option>
@@ -53,22 +25,15 @@
             <input type="number" class="form-control" min="0" name="tamanho1" id="tamanho1" placeholder="Tamanho" />
         </div>
 
-
-        <!-- <div class="form-group">
-            <div class="col-xs-5 col-xs-offset-1"></div>
-        </div> -->
-
         <div>
             <button type="button" class="my_button_plus"><i class="glyphicon glyphicon-plus"></i></button>
             <button type="button" class="my_button_minus"><i class="glyphicon glyphicon-minus"></i></button>
-            
-            
         </div>
         
     </div>
     
     <div>
-        <div class="col-md-5"><button type="submit" class="btn btn-default">Submit</button></div>
+        <div class="col-md-5"><button type="submit" class="btn btn-info">Concluir</button></div>
     </div>    
     
     <input type="hidden" name="contador" id="contador" value="1">
@@ -81,21 +46,24 @@
         var div_tamanho = $(".col-xs-2");
         var add_button = $(".my_button_plus");
         var remove_btn = $(".my_button_minus");
+        var div = document.getElementById("div_atributos");
         var cont = 2;
 
         $(add_button).click(function(e){
             e.preventDefault();
-            $(div_nome).append('<br><input type="text" class="form-control" name="nome'+cont+'" id="nome'+cont+'"  placeholder="Nome"/>');
 
-            $(div_tipo).append('<br><select class="form-control" id="tipo" name="tipo'+cont+'" id="tipo'+cont+'"><option>INT</option><option>VARCHAR[ ]</option><option>BOOLEAN</option><option>FLOAT</option><option>BLOB</option>');
+            $(div).append('<div class="col-md-6"> <input type="text" class="form-control" name="nome'+cont+'" id="nome'+cont+'" placeholder="Nome"/> </div>');
 
-            $(div_tamanho).append('<br><input type="number" class="form-control" name="tamanho'+cont+'" id="tamanho'+cont+'" placeholder="Tamanho"/>');
+            $(div).append('<div class="col-xs-3"> <select class="form-control" id="tipo'+cont+'" name="tipo'+cont+'"> <option>INT</option> <option>VARCHAR[ ]</option>  <option>BOOLEAN</option> <option>FLOAT</option> <option>BLOB</option>  </select> </div>');
+
+            $(div).append('<div class="col-xs-2"> <input type="number" class="form-control" min="0" name="tamanho'+cont+'" id="tamanho'+cont+'" placeholder="Tamanho" /> </div>');
+
             document.getElementById("contador").value = cont;
             $contador = cont;
             cont++;
         });
 
-       /* $(remove_btn).click(function(e){
+        $(remove_btn).click(function(e){
             if(cont > 2){   
                 cont--;
                 var nome = document.getElementById("nome"+cont);
@@ -106,6 +74,6 @@
                 nome.parentNode.removeChild(nome);
                 tamanho.parentNode.removeChild(tamanho);
             }
-        });*/
+        });
     });
 </script>
